b828d944316282861c10ee240c2ed9f6f85060c9
alinaT <alina.t@gosh.sh>
2025-08-06 00:27:49 +0300

    target remove

--- bindings/rust/target/release/build/libc-21915da6ed91f872/build_script_build-21915da6ed91f872.d
+++
@@ -1,5 +0,0 @@
-/Users/alinat/ZKLOGIN/BLSSS_REPOS/gosh_blst/bindings/rust/target/release/build/libc-21915da6ed91f872/build_script_build-21915da6ed91f872.d: /Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.174/build.rs
-
-/Users/alinat/ZKLOGIN/BLSSS_REPOS/gosh_blst/bindings/rust/target/release/build/libc-21915da6ed91f872/build_script_build-21915da6ed91f872: /Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.174/build.rs
-
-/Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.174/build.rs:
--- bindings/rust/target/release/deps/zeroize-0cc120448dd974a4.d
+++
@@ -1,8 +0,0 @@
-/Users/alinat/ZKLOGIN/BLSSS_REPOS/gosh_blst/bindings/rust/target/release/deps/zeroize-0cc120448dd974a4.d: /Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.1/src/lib.rs /Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.1/src/aarch64.rs
-
-/Users/alinat/ZKLOGIN/BLSSS_REPOS/gosh_blst/bindings/rust/target/release/deps/libzeroize-0cc120448dd974a4.rlib: /Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.1/src/lib.rs /Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.1/src/aarch64.rs
-
-/Users/alinat/ZKLOGIN/BLSSS_REPOS/gosh_blst/bindings/rust/target/release/deps/libzeroize-0cc120448dd974a4.rmeta: /Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.1/src/lib.rs /Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.1/src/aarch64.rs
-
-/Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.1/src/lib.rs:
-/Users/alinat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.1/src/aarch64.rs:
