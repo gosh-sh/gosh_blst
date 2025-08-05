716b0566056dda537e70f2bade7b606c0fd73c27
alinaT <alina.t@gosh.sh>
2025-08-06 00:50:40 +0300

    update cargo toml

--- bindings/rust/publish.sh
+++ bindings/rust/publish.sh
@@ -3,9 +3,9 @@
 HERE=`dirname $0`
 cd "${HERE}"
 
-if [ ! -d blst ]; then
-    trap '[ -h blst ] && rm -f blst' 0 2
-    ln -s ../.. blst
+if [ ! -d gosh_blst ]; then
+    trap '[ -h gosh_blst ] && rm -f gosh_blst' 0 2
+    ln -s ../.. gosh_blst
 fi
 
 # --allow-dirty because the temporary blst symbolic link is not committed
---
+++ bindings/rust/target/release/build/gosh_blst-a5b2dc85c8a3828c/build_script_build-a5b2dc85c8a3828c.d
@@ -0,0 +1,5 @@
+/Users/alinat/ZKLOGIN/BLSSS_REPOS/gosh_blst/bindings/rust/target/release/build/gosh_blst-a5b2dc85c8a3828c/build_script_build-a5b2dc85c8a3828c.d: build.rs
+
+/Users/alinat/ZKLOGIN/BLSSS_REPOS/gosh_blst/bindings/rust/target/release/build/gosh_blst-a5b2dc85c8a3828c/build_script_build-a5b2dc85c8a3828c: build.rs
+
+build.rs:
